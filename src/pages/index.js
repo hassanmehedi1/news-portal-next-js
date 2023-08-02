61de0555c5042cefc815e59f41654a764487ffd1
Mehedi Hassan <rehan51322@gmail.com>
2023-08-02 14:58:43 +0600

    backend data added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,30 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
+// import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
+import { useGetNewsesQuery } from "@/redux/api/api";
+import dynamic from "next/dynamic";
 
 const HomePage = ({ allNews }) => {
-  // console.log(allNews);
+  const { data, isLoading, isError, error } = useGetNewsesQuery();
+
+  const DynamicBanner = dynamic(() => import("@/components/UI/Banner"), {
+    loading: () => <h1>Loading...</h1>,
+    ssr: false,
+  });
+
   return (
     <>
       <Head>
-        <title>PH-News Portal</title>
+        <title>News Portal</title>
         <meta
           name="description"
-          content="This is news portal of programming hero made by next-js"
+          content="This is news portal of  made by next-js"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
+      <DynamicBanner />
       <AllNews allNews={allNews} />
     </>
   );
@@ -27,6 +35,22 @@ HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
+//SSR
+export const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:3000/api/news");
+
+  const data = await res.json();
+
+  // console.log(data);
+  return {
+    props: {
+      allNews: data?.data,
+    },
+  };
+};
+
+// SSG
+/*
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:5004/news");
 
@@ -40,3 +64,4 @@ export const getStaticProps = async () => {
     revalidate: 10,
   };
 };
+*/
--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,10 +1,73 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import { Col, Row } from "antd";
+import Image from "next/image";
+import {
+  ArrowRightOutlined,
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
 
 const NewsDetailPage = ({ news }) => {
   return (
     <div>
-      <h1>{news?.title}</h1>
-      <p>ID: {news?.id}</p>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={12}>
+          <div>
+            <Image
+              src={news?.image_url}
+              width={500}
+              height={300}
+              responsive
+              alt="news image"
+            />
+          </div>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <h1 style={{ fontSize: "25px" }}>{news?.title}</h1>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
+
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
+
+            <p style={{ fontSize: "20px" }}>{news?.description}</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -15,6 +78,22 @@ NewsDetailPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
+//SSR
+export const getServerSideProps = async (context) => {
+  const { params } = context;
+  const res = await fetch(`http://localhost:5004/news/${params.newsId}`);
+
+  const data = await res.json();
+
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
+
+// SSG
+/*
 export const getStaticPaths = async () => {
   const res = await fetch("http://localhost:5004/news");
 
@@ -39,3 +118,5 @@ export const getStaticProps = async (context) => {
     },
   };
 };
+
+*/
